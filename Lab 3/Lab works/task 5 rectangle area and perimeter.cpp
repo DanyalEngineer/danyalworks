d5f061e482a3a192880bbb22e90b9f07e9b2ee9d
Danyal Hassan <danyalhassan157@gmail.com>
2026-03-11 00:31:20 +0500

    Add files via upload

---
+++ Lab 3/Lab works/task 5 rectangle area and perimeter.cpp
@@ -0,0 +1,36 @@
+// Name: Danyal Hassan
+// Reg No: BF25NWELE0750
+// Program: Area and Perimeter of Rectangle
+
+#include <iostream>        
+using namespace std;       
+int main()                 
+    {
+    	
+	float length, width;   
+	// Variables to store length and width of rectangle
+    float area, perimeter; 
+	// Variables to store calculated area and perimeter
+
+    // Ask user to enter the length of the rectangle
+    cout << "Enter the length of rectangle: ";
+    cin >> length;         // Take length value from user
+
+    // Ask user to enter the width of the rectangle
+    cout << "Enter the width of rectangle: ";
+    cin >> width;          // Take width value from user
+
+    // Calculate the area using formula: Area = length × width
+    area = length * width;
+
+    // Calculate the perimeter using formula: 2 × (length + width)
+    perimeter = 2 * (length + width);
+
+    // Display calculated area
+    cout << "Area of rectangle = " << area << endl;
+
+    // Display calculated perimeter
+    cout << "Perimeter of rectangle = " << perimeter << endl;
+
+    return 0;              // Program ends successfully
+}
---
+++ Lab 3/Lab works/task 7 linear equation.cpp
@@ -0,0 +1,23 @@
+// Name: Danyal Hassan
+// Reg No: BF25NWELE0750
+#include <iostream>      
+// Library for input/output operations
+using namespace std;     
+// Allows using cout and cin without std::
+
+int main() 
+{
+    double x, y;         
+
+    // Ask the user to enter a value for x
+    cout << "Enter value of x: ";
+    cin >> x;           
+
+    // Calculate y using the linear equation y = 2x + 4
+    y = 2 * x + 4;
+
+    // Display the result
+    cout << "Value of y = " << y << endl;
+
+    return 0;            
+}
